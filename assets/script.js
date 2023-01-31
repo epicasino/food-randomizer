920d94bbba7611d65f1a612363ea9f58513a4ffa
epicasino2 <rayjonathanbadua@gmail.com>
2023-01-31 11:08:47 -0800

    script.js added and linked to index.html, arrays for ingredients that would be chosen at random from a list, results of randomization would be logged in console

--- assets/script.js
+++ assets/script.js
@@ -3,17 +3,33 @@ const noodles = [
     'Conchiglie', 'Macaroni', 'Penne', 'Rigatoni',
 ];
 
+let randomNoodle = noodles[Math.floor(Math.random() * noodles.length)];
+
+console.log(randomNoodle);
+
 const sauces = [
     'Alfredo', 'Bolognese', 'Carbonara', 'Marinara', 
     'Pomodoro', 'Pesto',
 ];
 
+let randomSauce = sauces[Math.floor(Math.random() * sauces.length)];
+
+console.log(randomSauce);
+
 const meats = [
     'Ground beef', 'Beef meatballs', 'Ground turkey',
     'Ground meatballs', 'Bacon', 'Cut sausage',
-]
+];
+
+let randomMeat = meats[Math.floor(Math.random() * meats.length)];
+
+console.log(randomMeat)
 
 const vegetables = [
     'Broccoli', 'Bell pepper', 'Zucchini',
     'Tomatoes', 'Garlic',
-]
+];
+
+let randomVegetable = vegetables[Math.floor(Math.random() * vegetables.length)];
+
+console.log(randomVegetable);
