42aeb22fdc9ccc4e8b67c8a0d46884451ff732a7
epicasino2 <rayjonathanbadua@gmail.com>
2023-01-31 05:15:31 -0800

    First bunch of code

---
+++ assets/script.js
@@ -0,0 +1,19 @@
+const noodles = [ 
+    'Fettucine', 'Angel Hair', 'Spaghetti', 
+    'Conchiglie', 'Macaroni', 'Penne', 'Rigatoni',
+];
+
+const sauces = [
+    'Alfredo', 'Bolognese', 'Carbonara', 'Marinara', 
+    'Pomodoro', 'Pesto',
+];
+
+const meats = [
+    'Ground beef', 'Beef meatballs', 'Ground turkey',
+    'Ground meatballs', 'Bacon', 'Cut sausage',
+]
+
+const vegetables = [
+    'Broccoli', 'Bell pepper', 'Zucchini',
+    'Tomatoes', 'Garlic',
+]
